bab00f38c430befe860d221fff8216acad045f35
khannaanurag1 <khannaanurag1@gmail.com>
2024-08-09 08:59:57 +0530

    Sending Email consumer

---
+++ src/main/java/org/example/emailservice_june2024/EmailServiceJune2024Application.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_june2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceJune2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceJune2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailservice_june2024/EmailServiceJune2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_june2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceJune2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
